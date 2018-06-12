61550af38c8d1fdb5236211b3a076e9931d0148b
iespino <iespino69@gmail.com>
2018-06-12 18:26:33 -0500

    Leer cookies

--- cookies/index.php
+++ cookies/index.php
@@ -1,4 +1,33 @@
-<?php
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+    <?php   
+    if(isset($_COOKIE['usuario']))
+      {
+        echo "Bienvenido ".$_COOKIE['usuario'];
+       }
+        else{echo "Bienvenido";}
+    ?> 
+    </title>
+</head>
+<body>
+
+    <form action="index.php" method="POST">
+        Nombre:
+        <input type="text" id="usuario" name="usuario" >
+        <button type="sumbit" name="enviar" id="enviar" >Enviar¡¡ </button>
+    </form>
+
+      <button type="submit" id="borrar" name="borrar">Borrar Cookie</button>
+
+      
+ <?php
 //Estructura de la cookie: //nombre = valor ; expires = fecha; path = /; domain= dominio.com
 
 if(isset($_POST['enviar']))
@@ -10,20 +39,6 @@ if(isset($_POST['enviar']))
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cookies</title>
-</head>
-<body>
-    <form action="index.php" method="POST">
-        Nombre:
-        <input type="text" id="usuario" name="usuario" >
-        <button type="sumbit" name="enviar" id="enviar" >Enviar¡¡ </button>
-    </form>
 </body>
 </html>
 
